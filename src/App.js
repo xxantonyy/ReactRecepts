718d0aed42fb5111a52979091c67ea5bc7cd7026
Anton <at0xa32@gmail.com>
2023-05-05 12:09:40 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { MealDetails } from './components/MealDetails';
 
 function App() {
    return <>
-      <Router>
+      <Router basename='/ReactRecepts'>
          <Header />
          <main className='container content'>
             <Routes>
